1d737f0bf18fd327e962de16596f55c8f5f88444
Jorge Mancilla <31830802+Jorgemn13@users.noreply.github.com>
2023-04-20 18:48:19 -0500

    Initial commit

---
+++ packages/frontend/src/pages/index.tsx
@@ -0,0 +1,62 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { CTA } from "../components/CTA";
+import { Footer } from "../components/Footer";
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero />
+    <Main>
+      <Text color="text">
+        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{" "}
+        <Code>TypeScript</Code>.
+      </Text>
+
+      <List spacing={3} my={0} color="text">
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          <ChakraLink
+            isExternal
+            href="https://chakra-ui.com"
+            flexGrow={1}
+            mr={2}
+          >
+            Chakra UI <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
+            Next.js <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+      </List>
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>
+        Fix issues on{" "}
+        <ChakraLink href="https://github.com/unapt/Vanadium" color="teal.500">
+          Github
+        </ChakraLink>
+        ❤️
+      </Text>
+    </Footer>
+    <CTA />
+  </Container>
+);
+
+export default Index;
